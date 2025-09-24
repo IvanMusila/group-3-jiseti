7320e4de554d4abe9e8451e601d566f02a3d52b5
anthony kiarie <akiarie27@gmail.com>
2025-09-24 21:32:14 +0300

    test: polyfill fetch APIs for jest

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,11 +1,33 @@
 import '@testing-library/jest-dom';
-<<<<<<< HEAD
+import { TextEncoder, TextDecoder } from 'util';
+import { fetch, Headers, Request, Response, FormData, File } from 'undici';
 import { server } from './mocks/server';
 
-// RTL/Jest setup
-// https://testing-library.com/docs/react-testing-library/setup/
+if (!globalThis.TextEncoder) {
+  globalThis.TextEncoder = TextEncoder;
+}
+if (!globalThis.TextDecoder) {
+  globalThis.TextDecoder = TextDecoder;
+}
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch;
+}
+if (!globalThis.Headers) {
+  globalThis.Headers = Headers;
+}
+if (!globalThis.Request) {
+  globalThis.Request = Request;
+}
+if (!globalThis.Response) {
+  globalThis.Response = Response;
+}
+if (!globalThis.FormData) {
+  globalThis.FormData = FormData;
+}
+if (!globalThis.File) {
+  globalThis.File = File;
+}
+
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
-=======
->>>>>>> develop
